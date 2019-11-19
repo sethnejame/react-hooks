a7d7173a31bfb6133d706832baf22a897ce0e190
Seth NeJame <seth@mondido.com>
2019-11-19 11:28:45 +0100

    Loading div now appears while API data loads

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ const App = () => {
   });
 
   const [query, setQuery] = useState("redux");
-  const [url, setUrl] = useState('https://hn.algolia.com/api/v1/search?query=redux');
+  const [url, setUrl] = useState(
+    "https://hn.algolia.com/api/v1/search?query=redux"
+  );
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -32,21 +34,27 @@ const App = () => {
           value={query}
           onChange={e => setQuery(e.target.value)}
         />
-        <button
-          type="button"
-          className="btn btn-primary mt-4"
-          onClick={() => setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)}
-        >
-          Submit
-        </button>
       </div>
-      <ul>
-        {data.hits.map((item, index) => (
-          <li key={index}>
-            <a href={item.url}>{item.title}</a>
-          </li>
-        ))}
-      </ul>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() =>
+          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
+        }
+      >
+        Submit
+      </button>
+      {isLoading ? (
+        <div>Loading. . .</div>
+      ) : (
+        <ul className="mt-4">
+          {data.hits.map((item, index) => (
+            <li key={index}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
